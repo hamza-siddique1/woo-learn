119b9378c8a93688e3b0ab951bf915c62e121b2f
Hamza Siddique <6793siddique@gmail.com>
2025-12-20 09:37:42 +0500

    add header and footer

--- functions.php
+++ functions.php
@@ -1,10 +1,2 @@
 <?php
 
-/* Loads parent stylesheet */
-add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
-function wpchild_enqueue_styles(){
-  wp_enqueue_style( 'wpm-twentytwentyfive-style', get_template_directory_uri() . '/style.css' );
-}
-
-
-
