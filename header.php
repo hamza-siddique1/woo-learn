1d52d6f9c1923e526b420497b44655395baa2b7a
Hamza Siddique <6793siddique@gmail.com>
2025-12-23 21:13:39 +0500

    show header elements from database

--- header.php
+++ header.php
@@ -34,7 +34,10 @@
             <div class="header-top">
                 <div class="container">
                     <div class="header-left">
-                        <a href="tel:#"><i class="icon-phone"></i>Call: +0123 456 789</a>
+                        <?php if( $phone_number = get_field('phone_number', 'option') ) : ?>
+                            <a href="tel:#"><i class="icon-phone"></i>Call: <?php echo $phone_number; ?></a>
+                        <?php endif; ?>
+
                     </div><!-- End .header-left -->
 
                     <div class="header-right">
@@ -50,18 +53,6 @@
                                                 <ul>
                                                     <li><a href="#">Eur</a></li>
                                                     <li><a href="#">Usd</a></li>
-                                                </ul>
-                                            </div><!-- End .header-menu -->
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="header-dropdown">
-                                            <a href="#">English</a>
-                                            <div class="header-menu">
-                                                <ul>
-                                                    <li><a href="#">English</a></li>
-                                                    <li><a href="#">French</a></li>
-                                                    <li><a href="#">Spanish</a></li>
                                                 </ul>
                                             </div><!-- End .header-menu -->
                                         </div>
@@ -84,7 +75,9 @@
                         </button>
 
                         <a href="index.html" class="logo">
-                            <img src="assets/images/demos/demo-4/logo.png" alt="Molla Logo" width="105" height="25">
+                            <?php if($header_logo = get_field('header_logo', 'options')) : ?>
+                                <img src="<?php echo $header_logo['url']; ?>" alt="<?php echo $header_logo['title']; ?>" width="105" height="25">
+                            <? endif; ?>
                         </a>
                     </div><!-- End .header-left -->
 
@@ -102,42 +95,6 @@
                     </div>
 
                     <div class="header-right">
-                        <div class="dropdown compare-dropdown">
-                            <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static" title="Compare Products" aria-label="Compare Products">
-                                <div class="icon">
-                                    <i class="icon-random"></i>
-                                </div>
-                                <p>Compare</p>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right">
-                                <ul class="compare-products">
-                                    <li class="compare-product">
-                                        <a href="#" class="btn-remove" title="Remove Product"><i class="icon-close"></i></a>
-                                        <h4 class="compare-product-title"><a href="product.html">Blue Night Dress</a></h4>
-                                    </li>
-                                    <li class="compare-product">
-                                        <a href="#" class="btn-remove" title="Remove Product"><i class="icon-close"></i></a>
-                                        <h4 class="compare-product-title"><a href="product.html">White Long Skirt</a></h4>
-                                    </li>
-                                </ul>
-
-                                <div class="compare-actions">
-                                    <a href="#" class="action-link">Clear All</a>
-                                    <a href="#" class="btn btn-outline-primary-2"><span>Compare</span><i class="icon-long-arrow-right"></i></a>
-                                </div>
-                            </div><!-- End .dropdown-menu -->
-                        </div><!-- End .compare-dropdown -->
-
-                        <div class="wishlist">
-                            <a href="wishlist.html" title="Wishlist">
-                                <div class="icon">
-                                    <i class="icon-heart-o"></i>
-                                    <span class="wishlist-count badge">3</span>
-                                </div>
-                                <p>Wishlist</p>
-                            </a>
-                        </div><!-- End .compare-dropdown -->
 
                         <div class="dropdown cart-dropdown">
                             <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
@@ -606,9 +563,13 @@
                         </nav><!-- End .main-nav -->
                     </div><!-- End .header-center -->
 
-                    <div class="header-right">
-                        <i class="la la-lightbulb-o"></i><p>Clearance<span class="highlight">&nbsp;Up to 30% Off</span></p>
-                    </div>
+                    <?php if ( $offer_text = get_field('offer_text', 'option') ) : ?>
+                        <div class="header-right">
+                            <i class="la la-lightbulb-o"></i>
+                            <?php echo $offer_text; ?>
+                        </div>
+                    <?php endif; ?>
+
                 </div><!-- End .container -->
             </div><!-- End .header-bottom -->
         </header><!-- End .header -->
---
+++ inc/functions/scripts.php
@@ -0,0 +1,73 @@
+<?php
+
+function mytheme_enqueue_styles() {
+
+    $theme_uri  = get_template_directory_uri();
+    $theme_dir  = get_template_directory();
+    $theme_slug = 'learning';
+
+    $files = array(
+        'line-awesome.min.css',
+        'bootstrap.min.css',
+        'owl.carousel.css',
+        'magnific-popup.css',
+        'jquery.countdown.css',
+        'style.css',
+        'skin-demo-4.css',
+        'demo-4.css',
+    );
+
+    foreach ( $files as $file ) {
+
+        $handle = $theme_slug . '-' . pathinfo( $file, PATHINFO_FILENAME );
+        $path   = '/assets/css/' . $file;
+
+        wp_enqueue_style(
+            $handle,
+            $theme_uri . $path,
+            array(),
+            filemtime( $theme_dir . $path ),
+            'all'
+        );
+    }
+}
+
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_styles' );
+
+function mytheme_enqueue_scripts() {
+
+    $theme_uri  = get_template_directory_uri();
+    $theme_dir  = get_template_directory();
+    $theme_slug = 'mytheme';
+
+    $files = array(
+        'jquery.min.js',
+        'bootstrap.bundle.min.js',
+        'jquery.hoverIntent.min.js',
+        'jquery.waypoints.min.js',
+        'superfish.min.js',
+        'owl.carousel.min.js',
+        'bootstrap-input-spinner.js',
+        'jquery.plugin.min.js',
+        'jquery.magnific-popup.min.js',
+        'jquery.countdown.min.js',
+        'main.js',
+        'demo-4.js',
+    );
+
+    foreach ( $files as $file ) {
+
+        $handle = $theme_slug . '-' . pathinfo( $file, PATHINFO_FILENAME );
+        $path   = '/assets/js/' . $file;
+
+        wp_enqueue_script(
+            $handle,
+            $theme_uri . $path,
+            array( 'jquery' ),
+            file_exists( $theme_dir . $path ) ? filemtime( $theme_dir . $path ) : null,
+            true
+        );
+    }
+}
+
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_scripts' );
